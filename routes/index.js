098fdf1aec78cfe177c73e164acad1416d11909b
jesse soldat <jsoldat@hotmail.com>
2016-08-16 12:28:25 +0700

    user can not go to the dashboard unless logged in

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express'),
 	router = express.Router();
 
-router.get('/', function(req, res){
+router.get('/', ensureAuth, function(req, res){
 	res.render('index')
 });
+
+
+function ensureAuth(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	} else {
+		res.redirect('/users/login');
+	}
+}
 
 
 module.exports = router;
